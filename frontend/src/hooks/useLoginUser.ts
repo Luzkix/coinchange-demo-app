5965ff2d23a4e6ad5592f22ebc40dc27f6497f13
Zdenek L. <66368980+Luzkix@users.noreply.github.com>
2025-06-10 21:05:25 +0200

    Frontend: - fixed issue when openApi was sending invalid jwt token to login endpoint which was rejected on backend, thus user could never log-in. Therefore for userLoginUser hook the OpenAPI.TOKEN will be always set to undefined to be sure such issue cannot happen again

--- frontend/src/hooks/useLoginUser.ts
+++ frontend/src/hooks/useLoginUser.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { useAuth } from '../contexts/AuthContext';
-import { ApiError } from '../api-generated/backend';
+import { ApiError, OpenAPI } from '../api-generated/backend';
 import { UserService } from '../services/UserService.ts';
 import { useProcessApiError } from './useProcessApiError.ts';
 
@@ -8,6 +8,8 @@ export const useLoginUser = () => {
   const { login } = useAuth();
   const processApiError = useProcessApiError();
   const processName = useLoginUser.name;
+
+  OpenAPI.TOKEN = undefined; //to be sure that old token is never handed over to login endpoint even by mistake
 
   return useMutation({
     mutationFn: UserService.login,
